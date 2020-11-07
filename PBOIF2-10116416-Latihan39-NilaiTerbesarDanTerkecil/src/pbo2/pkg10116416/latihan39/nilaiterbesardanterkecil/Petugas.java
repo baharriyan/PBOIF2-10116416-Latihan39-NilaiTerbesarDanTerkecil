4ebbdfb564b916ada9d6fd2886f67fa784556771
baharriyan <indonesiariyan@gmail.com>
2020-11-08 05:57:34 +0700

    first commit

---
+++ PBOIF2-10116416-Latihan39-NilaiTerbesarDanTerkecil/src/pbo2/pkg10116416/latihan39/nilaiterbesardanterkecil/Petugas.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116416.latihan39.nilaiterbesardanterkecil;
+
+/**
+ * 
+ * @author  
+ * NAMA     : riyan bahar
+ * KELAS    : IF-2
+ * NIM      : 10116416
+ */
+public class Petugas {
+    private String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
